4c20a5f7f01c40c00ff988b9a1b1669e099eb222
seangabrielclim-lab <sean_gabriel_c_lim@dlsu.edu.ph>
2026-06-22 23:16:49 +0800

    Update sort.h

--- sort.h
+++ sort.h
@@ -9,7 +9,7 @@
 
     JUGNO, DANIEL CHRISTOPHER  S04
     LIM,   SEAN GABRIEL        S09
-    CHUA,  HANELLE CELINNE     S09 
+    CHUA,  HANNELLE CELINNE     S09 
 
     PURPOSE OF THIS FILE: header file for the sorting algorithms
 
