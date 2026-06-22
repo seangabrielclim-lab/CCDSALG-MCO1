d545a23d2d1e2c34475b86e7cb5ae02528ad69df
seangabrielclim-lab <sean_gabriel_c_lim@dlsu.edu.ph>
2026-06-22 19:39:29 +0800

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,3 +1,21 @@
+/*
+
+    >> Encode your group number (2 digits) and last names, first names and sections of group members.  Encode the purpose of this file.
+
+    Group #: 09  (2 digits)
+
+    JUGNO, DANIEL CHRISTOPHER  S04
+    LIM,   SEAN GABRIEL        S09
+    CHUA,  HANELLE CELINNE     S09 
+
+    PURPOSE OF THIS FILE: header file for the sort files
+
+    >> GENERAL INSTRUCTIONS:
+    1. Adhere with good programming style and practice (learned from CCPROG1 and CCPROG2).
+    3. Do NOT use global variables!  NON-COMPLIANCE WILL RESULT INTO A 10 POINT DEDUCTION!
+*/
+
+
 #ifndef SORT_H
 #define SORT_H
 
