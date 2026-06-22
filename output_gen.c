5578b1109ce4464075abb26f7b994ca32f22d662
seangabrielclim-lab <sean_gabriel_c_lim@dlsu.edu.ph>
2026-06-22 19:42:41 +0800

    Update output_gen.c

--- output_gen.c
+++ output_gen.c
@@ -1,3 +1,21 @@
+/*
+
+    >> Encode your group number (2 digits) and last names, first names and sections of group members.  Encode the purpose of this file.
+
+    Group #: 09  (2 digits)
+
+    JUGNO, DANIEL CHRISTOPHER  S04
+    LIM,   SEAN GABRIEL        S09
+    CHUA,  HANELLE CELINNE     S09 
+
+    PURPOSE OF THIS FILE: generated the datasets of sizes 2^6 to 2^15
+
+    >> GENERAL INSTRUCTIONS:
+    1. Adhere with good programming style and practice (learned from CCPROG1 and CCPROG2).
+    3. Do NOT use global variables!  NON-COMPLIANCE WILL RESULT INTO A 10 POINT DEDUCTION!
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
