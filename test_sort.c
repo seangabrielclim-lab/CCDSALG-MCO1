de63f45fc44da4a53cdc9b2db9ca3050e621de56
seangabrielclim-lab <sean_gabriel_c_lim@dlsu.edu.ph>
2026-06-22 11:47:32 +0800

    Update test_sort.c

--- test_sort.c
+++ test_sort.c
@@ -1,3 +1,21 @@
+/**
+    >> Encode your group number (2 digits) and last names, first names and sections of group members.  Encode the purpose of this file.
+
+    Group #: 09  (2 digits)
+
+    JUGNO, DANIEL CHRISTOPHER  S04
+    LIM,   SEAN GABRIEL        S09
+    CHUA,  HANELLE CELINNE     S09 
+
+    PURPOSE OF THIS FILE: to test and call the stack operations defined in stack.c
+
+    >> GENERAL INSTRUCTIONS: NON-COMPLIANCE WILL RESULT INTO AT LEAST A 10 POINT DEDUCTION!
+    1. Adhere with good programming style and practice (learned from CCPROG1 and CCPROG2).
+    2. Do NOT use global variables!  
+    3. Do NOT use goto statement!
+    4. Do NOT use return in a void function!
+*/
+
 #include <stdio.h>
 #include "sort.c"
 
