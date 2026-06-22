7427ae40e79e61020048d02ba2d53c10e493f90f
seangabrielclim-lab <sean_gabriel_c_lim@dlsu.edu.ph>
2026-06-22 23:17:40 +0800

    Update test_slow.c

--- test_slow.c
+++ test_slow.c
@@ -6,7 +6,7 @@
 
     JUGNO, DANIEL CHRISTOPHER  S04
     LIM,   SEAN GABRIEL        S09
-    CHUA,  HANELLE CELINNE     S09 
+    CHUA,  HANNELLE CELINNE     S09 
 
     PURPOSE OF THIS FILE: tests the implementation of the slow version of the Graham scan algorithm
 
