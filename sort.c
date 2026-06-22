d98979ffbad607f12eef3789e1cacbdca19ced98
seangabrielclim-lab <sean_gabriel_c_lim@dlsu.edu.ph>
2026-06-22 09:44:04 +0800

    Update sort.c

--- sort.c
+++ sort.c
@@ -22,10 +22,14 @@
 #include <stdbool.h>
 #include "sort.h"
 
-// points[] is the set of points
-// n is the number of elements in the points array
-// returns the anchor point (lowest y value point, and the leftmost instead incase of two equal bottom most y values)
-
+/*
+    a. Name of Programmer(s):  Jugno, Daniel Christopher
+    b. Name of Tester(s)    :  Lim, Sean
+    c. Code Type -- 100% Human Generated 
+    d. Purpose: this function will search for the anchor point
+    e. Return: returns the anchor point (lowest y value point, and the leftmost instead incase of two equal bottom most y values)
+    f. Parameters: points[] is the set of points, n is the number of elements in the points array
+*/
 Point searchAnchor(Point points[], int n){
 	Point anchor = points[0]; // assumes first element is the anchor
 	int i;
@@ -40,25 +44,39 @@ Point searchAnchor(Point points[], int n){
 	return anchor;
 }
 
-// p0 is anchor point
-// a is another point
-// returns the angle formed by a point and the anchor point, in reference to the horizontal x axis
+/*
+    a. Name of Programmer(s):  Jugno, Daniel Christopher
+    b. Name of Tester(s)    :  Lim, Sean
+    c. Code Type -- 100% Human Generated 
+    d. Purpose: this function will calculate the angle between the anchor point and a another given point
+    e. Return: returns the angle formed by a point and the anchor point, in reference to the horizontal x axis
+    f. Parameters: p0 is anchor point, a is another point
+*/
 double computeAngle(Point p0, Point a){
 	return atan2((a.y - p0.y), (a.x - p0.x));
 }
 
+/*
+    a. Name of Programmer(s):  Jugno, Daniel Christopher
+    b. Name of Tester(s)    :  Lim, Sean
+    c. Code Type -- 100% Human Generated 
+    d. Purpose: this function will compute the distance between two points
+    e. Return: returns the distance between the two points a and b
+    f. Parameters: a is the first point, b is the second point 
+*/
 double computeDistance(Point a, Point b){
 	double distance = sqrt(((a.x - b.x)*(a.x - b.x)) + ((a.y - b.y)*(a.y - b.y))); // dist between 2 points formula
 	return distance;
 }
 
-// p0 is anchor point
-// a is another point
-// b is another point
-// compares point a with point b to see which forms a smaller angle with p0
-// returns true if point a is smaller
-// false if point b is smaller
-// incase points are collinear, returns true if point a is closer in distance to p0
+/*
+    a. Name of Programmer(s):  Jugno, Daniel Christopher
+    b. Name of Tester(s)    :  Lim, Sean
+    c. Code Type -- 100% Human Generated 
+    d. Purpose: this function will compare two points a and b by the angle they form with p0
+    e. Return: returns true if a forms a smaller angle with p0, false if b forms a smaller angle with p0. In case points are collinear, returns true if point a is closer in distance to p0
+    f. Parameters: p0 is anchor point, a and b are the points being compared with p0
+*/
 bool comparePoints(Point p0, Point a, Point b){
 	
 	double angleA, angleB;
@@ -72,6 +90,14 @@ bool comparePoints(Point p0, Point a, Point b){
 	return angleA < angleB;
 }
 
+/*
+    a. Name of Programmer(s):  Jugno, Daniel Christopher
+    b. Name of Tester(s)    :  Lim, Sean
+    c. Code Type -- 100% Human Generated 
+    d. Purpose: this function will sort the array of points using selection sort, excluding the anchor point
+    e. Return: none
+    f. Parameters: p0 is the anchor point, points is the array of points, n is the number of elements in the array
+*/
 void selectionSort(Point points[], Point p0, int n){ // selection sort algorithm which sorts the array not containing the anchor point
 	int i, j, min;									 // based on each point's angle formed with the anchor point, with respect to the horizontal x axis
 	Point temp;										 // collinear points are sorted by their dis
@@ -91,7 +117,14 @@ void selectionSort(Point points[], Point p0, int n){ // selection sort algorithm
 	}
 }
 
-
+/*
+    a. Name of Programmer(s):  Chua, Hannelle
+    b. Name of Tester(s)    :  Lim, Sean
+    c. Code Type -- 100% Human Generated 
+    d. Purpose: this function will find the pivot point in preparation for quick sort algorithm
+    e. Return: returns the index of the pivot point posiiton
+    f. Parameters: p0 is the anchor point, points is the array of points, low is the lower boundary, high is the upper boundary
+*/
 int partition(Point points[], Point p0, int low, int high)
 {
     Point pivot = points[high];   // choose the last point as pivot
@@ -121,6 +154,14 @@ int partition(Point points[], Point p0, int low, int high)
     return i + 1; // return pivot position
 }
 
+/*
+    a. Name of Programmer(s):  Chua, Hannelle
+    b. Name of Tester(s)    :  Lim, Sean
+    c. Code Type -- 100% Human Generated 
+    d. Purpose: this function will sort the points using a quick sort algorithm
+    e. Return: none
+    f. Parameters: p0 is the anchor point, points is the array of points, low is the lower boundary, high is the upper boundary
+*/
 void quickSort(Point points[], Point p0, int low, int high)
 {
     int pivotIndex;
