5b6588fd3d0c614a6eba0a09a95d05ced444f745
seangabrielclim-lab <sean_gabriel_c_lim@dlsu.edu.ph>
2026-06-22 23:16:23 +0800

    Update output_gen.c

--- output_gen.c
+++ output_gen.c
@@ -6,7 +6,7 @@
 
     JUGNO, DANIEL CHRISTOPHER  S04
     LIM,   SEAN GABRIEL        S09
-    CHUA,  HANELLE CELINNE     S09 
+    CHUA,  HANNELLE CELINNE     S09 
 
     PURPOSE OF THIS FILE: generated the datasets of sizes 2^6 to 2^15
 
